5c6a7be39122cca5fc548c41238a76f6d36f4852
Aarthi-G2002 <aarthi.g02@gmail.com>
2025-05-26 22:03:52 +0530

    Initial commit: Added HTML, CSS, JS, and assets

---
+++ script.js
@@ -0,0 +1,11 @@
+document.getElementById('messageBtn').addEventListener('click',function()
+{
+
+    document.getElementById('name').value = '';
+    document.getElementById('mail').value = '';
+    document.getElementById('no').value = '';
+    document.getElementById('text').value = '';
+    document.getElementById('textbox').value = '';
+
+});
+
